c98b52b0ff5fb516987bcb6df98ad9d7643ba200
David <grikurovdavid@gmail.com>
2024-09-18 16:12:31 +0200

    update

--- src/app/api/crypto-data/route.ts
+++ src/app/api/crypto-data/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   try {
-    const url = 'http://localhost:5000/api/cryptos';
+    const url = 'https://twenty-pugs-invite.loca.lt/api/cryptos';
     const res = await fetch(url, {
       method: 'GET',
       headers: {
--- src/app/api/global-data/route.ts
+++ src/app/api/global-data/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   try {
-    const url = 'http://localhost:5000/api/global';
+    const url = 'https://twenty-pugs-invite.loca.lt/api/global';
 
     const res = await fetch(url, {
       method: 'GET'
