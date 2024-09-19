5cfa597e59c56db286de6d08dad97babcd1fcfd0
David <grikurovdavid@gmail.com>
2024-09-19 18:06:28 +0200

    update

--- src/store/crypto/crypto.store.ts
+++ src/store/crypto/crypto.store.ts
@@ -2,37 +2,42 @@ import { create } from 'zustand'
 import { useEffect } from 'react'
 
 interface ICryptoStore {
-  favorites: string[];
-  addFavorite: (id: string) => void;
-  removeFavorite: (id: string) => void;
-  initializeFavorites: () => void;
+  favorites: string[]
+  isLoading: boolean 
+  addFavorite: (id: string) => void
+  removeFavorite: (id: string) => void
+  initializeFavorites: () => void
 }
 
 export const useCryptoStore = create<ICryptoStore>((set) => ({
   favorites: [],
+  isLoading: true,
 
   initializeFavorites: () => {
-    const storedFavorites = JSON.parse(localStorage.getItem('favorites') || '[]');
-    set({ favorites: storedFavorites });
+    set({ isLoading: true })
+
+    const storedFavorites = JSON.parse(localStorage.getItem('favorites') || '[]')
+
+    set({ favorites: storedFavorites, isLoading: false })
   },
 
   addFavorite: (id: string) => set((state) => {
-    const updatedFavorites = Array.from(new Set([...state.favorites, id]));
-    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-    return { favorites: updatedFavorites };
+    const updatedFavorites = Array.from(new Set([...state.favorites, id]))
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites))
+    return { favorites: updatedFavorites }
   }),
 
   removeFavorite: (id: string) => set((state) => {
-    const updatedFavorites = state.favorites.filter((fav) => fav !== id);
-    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-    return { favorites: updatedFavorites };
+    const updatedFavorites = state.favorites.filter((fav) => fav !== id)
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites))
+    return { favorites: updatedFavorites }
   })
-}));
+}))
 
 export const useInitializeCryptoStore = () => {
-  const initializeFavorites = useCryptoStore((state) => state.initializeFavorites);
+  const initializeFavorites = useCryptoStore((state) => state.initializeFavorites)
 
   useEffect(() => {
-    initializeFavorites();
-  }, [initializeFavorites]);
-};
+    initializeFavorites()
+  }, [initializeFavorites])
+}
