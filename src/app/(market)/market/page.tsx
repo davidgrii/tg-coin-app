5851ae4b6e23d0f46b42bd6c3c937ef4de1c2132
David <grikurovdavid@gmail.com>
2024-09-20 21:56:52 +0200

    update

--- src/app/(market)/market/page.tsx
+++ src/app/(market)/market/page.tsx
@@ -1,54 +1,83 @@
 'use client'
 
-import { Container, CryptoItem, CryptoSkeleton, CryptoTableHeader } from '@/components'
 import { useEffect, useRef, useState } from 'react'
 import { useCryptoStore, useInitializeCryptoStore, useSearchStore } from '@/store'
-import { Card } from '@/components/ui/card'
-import { motion } from 'framer-motion'
 import { useCrypto, useCryptoFilter } from '@/hooks'
+import { Container, CryptoItem, CryptoSkeleton, CryptoTableHeader } from '@/components'
+import { motion } from 'framer-motion'
+import { Card } from '@/components/ui/card'
 import { SearchInput } from '@/components/market'
 
 export default function MarketPage() {
-  useInitializeCryptoStore()
+  useInitializeCryptoStore();
 
-  const [searchValue, setSearchValue] = useState('')
-  const inputRef = useRef<HTMLInputElement | null>(null)
+  const [searchValue, setSearchValue] = useState('');
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const { favorites, addFavorite, removeFavorite } = useCryptoStore()
-  const { isSearchOpen } = useSearchStore()
+  const { favorites, addFavorite, removeFavorite } = useCryptoStore();
+  const { isSearchOpen } = useSearchStore();
+  const { cryptoData = [], isLoading } = useCrypto();
+  const { filteredCryptoData } = useCryptoFilter(cryptoData, searchValue);
 
-  const { cryptoData = [], isLoading } = useCrypto()
-  const { filteredCryptoData } = useCryptoFilter(cryptoData, searchValue)
+  const [loadingStartTime, setLoadingStartTime] = useState<number | null>(null);
+  const [itemsToShow, setItemsToShow] = useState(10);
+  const [isEndOfList, setIsEndOfList] = useState(false);
+
+  useEffect(() => {
+    if (isLoading && loadingStartTime === null) {
+      setLoadingStartTime(Date.now());
+    }
+    if (!isLoading) {
+      setLoadingStartTime(null);
+    }
+  }, [isLoading, loadingStartTime]);
+
+  const isShortLoading = loadingStartTime && (Date.now() - loadingStartTime < 200);
 
   useEffect(() => {
     if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
-      const bot = window.Telegram.WebApp
-
-      bot.ready()
-      bot.setHeaderColor('#000')
-      bot.setBackgroundColor('#000')
-      bot.setBottomBarColor('#000')
-
-      bot.isVerticalSwipesEnabled = false
-
+      const bot = window.Telegram.WebApp;
+      bot.ready();
+      bot.setHeaderColor('#000');
+      bot.setBackgroundColor('#000');
+      bot.setBottomBarColor('#000');
+      bot.isVerticalSwipesEnabled = false;
       if (!bot.isExpanded) {
-        bot.expand()
+        bot.expand();
       }
     } else {
-      document.body.style.backgroundColor = '#000'
+      document.body.style.backgroundColor = '#000';
     }
-  }, [])
+  }, []);
+
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight - 200
+    ) {
+      if (itemsToShow < filteredCryptoData.length) {
+        setItemsToShow((prev) => prev + 10);
+      } else {
+        setIsEndOfList(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [itemsToShow, filteredCryptoData.length]);
 
   return (
     <Container className={'pt-0 mb-20'}>
-
-      {isSearchOpen &&
+      {isSearchOpen && (
         <SearchInput
           searchValue={searchValue}
           setSearchValue={setSearchValue}
           inputRef={inputRef}
         />
-      }
+      )}
 
       <CryptoTableHeader />
 
@@ -57,7 +86,7 @@ export default function MarketPage() {
         animate={{ opacity: 1 }}
         transition={{ duration: 0.7 }}
       >
-        {isLoading ?
+        {isLoading && !isShortLoading ? (
           <motion.div
             key="skeletons"
             initial={{ opacity: 1 }}
@@ -70,7 +99,9 @@ export default function MarketPage() {
               <CryptoSkeleton key={index} />
             ))}
           </motion.div>
-          :
+        ) : null}
+
+        {!isLoading ? (
           <motion.div
             key="crypto-items"
             initial={{ opacity: 0 }}
@@ -79,22 +110,20 @@ export default function MarketPage() {
             transition={{ duration: 0.5 }}
           >
             <Card className={'bg-background grid gap-8 border-0'}>
-              {filteredCryptoData.length > 0 && (
-                filteredCryptoData.map((crypto, index) => (
-                  <CryptoItem
-                    key={crypto.id}
-                    crypto={crypto}
-                    index={index}
-                    favorites={favorites}
-                    addFavorite={addFavorite}
-                    removeFavorite={removeFavorite}
-                  />
-                ))
-              )}
+              {filteredCryptoData.slice(0, itemsToShow).map((crypto, index) => (
+                <CryptoItem
+                  key={crypto.id}
+                  crypto={crypto}
+                  index={index}
+                  favorites={favorites}
+                  addFavorite={addFavorite}
+                  removeFavorite={removeFavorite}
+                />
+              ))}
             </Card>
           </motion.div>
-        }
+        ) : null}
       </motion.div>
     </Container>
-  )
+  );
 }
