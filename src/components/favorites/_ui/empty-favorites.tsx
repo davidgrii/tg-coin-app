33270a3071090f4bfd11133b41102aba7342d674
David <grikurovdavid@gmail.com>
2024-09-21 18:25:07 +0200

    update

--- src/components/favorites/_ui/empty-favorites.tsx
+++ src/components/favorites/_ui/empty-favorites.tsx
@@ -14,7 +14,7 @@ export const EmptyFavorites: React.FC<IProps> = ({ isFavoritesEmpty, className }
   if (isFavoritesEmpty) {
     return (
       <motion.div
-        className={'flex flex-col h-[300px] items-center justify-center text-center'}
+        className={'flex flex-col h-[300px] items-center text-sm justify-center text-center'}
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5, ease: 'easeOut' }}
