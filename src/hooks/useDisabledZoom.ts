461ef9988e12acdeaf40e1b71f786d8a8ec1047c
David <grikurovdavid@gmail.com>
2024-10-08 14:20:22 +0200

    update

--- src/hooks/useDisabledZoom.ts
+++ src/hooks/useDisabledZoom.ts
@@ -2,18 +2,25 @@ import { useEffect } from 'react'
 
 const useDisableZoom = () => {
   useEffect(() => {
-    const handleFocus = (event) => {
-      // Устанавливаем стиль для предотвращения зума
-      event.target.style.fontSize = '16px';
+    const handleFocus = (event: FocusEvent) => {
+      // Проверяем, что целевой элемент является инпутом или текстовой областью
+      const target = event.target as HTMLInputElement | HTMLTextAreaElement;
+      if (target) {
+        // Устанавливаем стиль для предотвращения зума
+        target.style.fontSize = '16px';
+      }
     };
 
-    const handleBlur = (event) => {
-      // Восстанавливаем оригинальный стиль после потери фокуса
-      event.target.style.fontSize = '';
+    const handleBlur = (event: FocusEvent) => {
+      const target = event.target as HTMLInputElement | HTMLTextAreaElement;
+      if (target) {
+        // Восстанавливаем оригинальный стиль после потери фокуса
+        target.style.fontSize = '';
+      }
     };
 
     // Добавляем обработчики для инпутов и текстовых областей
-    const inputs = document.querySelectorAll('input, textarea');
+    const inputs = document.querySelectorAll<HTMLInputElement | HTMLTextAreaElement>('input, textarea');
     inputs.forEach(input => {
       input.addEventListener('focus', handleFocus);
       input.addEventListener('blur', handleBlur);
