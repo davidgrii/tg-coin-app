47b982c2896719751afed7efaad101cbbf69e68e
David <grikurovdavid@gmail.com>
2024-10-12 14:38:51 +0200

    update

--- src/app/(market)/layout.tsx
+++ src/app/(market)/layout.tsx
@@ -9,11 +9,11 @@ export default function Layout({ children }: {
   return (
     <>
       <StrictMode>
-        <body className={'overflow-y-auto h-full'}>
+        <div className={'overflow-y-auto h-full'}>
           <Dashboard variant={'public'} />
           {children}
           <NavMenu />
-        </body>
+        </div>
       </StrictMode>
     </>
   )
