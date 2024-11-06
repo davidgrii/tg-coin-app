cdb44a1acb773b8e6b96b39d0a0e968b27e23f1b
David <grikurovdavid@gmail.com>
2024-11-06 15:44:45 +0100

    update

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -31,7 +31,7 @@ export const Categories: React.FC<IProps> = ({ className }) => {
 
       container.scrollTo({
         left: scrollPosition,
-        behavior: 'smooth'
+        behavior: 'auto'
       })
     }
   }, [currentPage])
