a8481a4f34837c4164b6a498cfa2e1a2da5d1b7d
David <grikurovdavid@gmail.com>
2024-11-25 13:19:04 +0100

    update

--- src/components/crypto-item-details.tsx
+++ src/components/crypto-item-details.tsx
@@ -5,19 +5,21 @@ import { formatPrice, getDynamicFontSize } from '@/utils/formatters'
 import { StarFavoriteIcon, StarIcon } from '@/components/icons'
 import { useCryptoModalStore } from '@/store/crypto/crypto-modal.store'
 import { useQuery } from '@tanstack/react-query'
+import { ICryptoDetails } from '@/types'
+import { DetailsCoinsData, DetailsMarketsData } from '@/components'
 import React, { useState } from 'react'
 import { CryptoModal } from '@/components/ui/crypto-modal'
 
-// const fetchCryptoDetailsData = async (id: string | undefined) => {
-//   if (!id) throw new Error('No crypto ID provided')
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/cryptos/${id}`)
-//
-//   if (!res.ok) {
-//     console.log('Failed to fetch crypto details')
-//   }
-//
-//   return res.json()
-// }
+const fetchCryptoDetailsData = async (id: string | undefined): Promise<ICryptoDetails> => {
+  if (!id) throw new Error('No crypto ID provided')
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/cryptos/${id}`)
+
+  if (!res.ok) {
+    console.log('Failed to fetch crypto details')
+  }
+
+  return res.json()
+}
 
 interface IProps {
   favorites: string[]
@@ -34,32 +36,32 @@ export const CryptoItemDetails: React.FC<IProps> = ({ userId, favorites, removeF
 
   const { data: detailsData } = useQuery({
     queryKey: ['cryptoDetails', selectedCrypto?.id],
-    // queryFn: () => fetchCryptoDetailsData(selectedCrypto?.id),
+    queryFn: () => fetchCryptoDetailsData(selectedCrypto?.id),
     staleTime: 30 * 60 * 1000,
     enabled: !!selectedCrypto
   })
 
-  if (!isOpen || !selectedCrypto) return null
+  if (!isOpen || !selectedCrypto || !detailsData) return null
 
   const cryptoPrice = selectedCrypto?.current_price || selectedCrypto?.price || 0
   const isFavorite = favorites.includes(selectedCrypto.id)
 
-  // const handleFavoriteToggle = async (event: React.MouseEvent) => {
-  //   event.stopPropagation()
-  //   setLoading(true)
-  //
-  //   try {
-  //     if (isFavorite) {
-  //       await removeFavorite(userId, selectedCrypto.id)
-  //     } else {
-  //       await addFavorite(userId, selectedCrypto.id)
-  //     }
-  //   } catch (error) {
-  //     console.error('Error toggling favorite:', error)
-  //   } finally {
-  //     setLoading(false)
-  //   }
-  // }
+  const handleFavoriteToggle = async (event: React.MouseEvent) => {
+    event.stopPropagation()
+    setLoading(true)
+
+    try {
+      if (isFavorite) {
+        await removeFavorite(userId, selectedCrypto.id)
+      } else {
+        await addFavorite(userId, selectedCrypto.id)
+      }
+    } catch (error) {
+      console.error('Error toggling favorite:', error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <CryptoModal isOpen={isOpen} onClose={closeModal}>
@@ -90,6 +92,7 @@ export const CryptoItemDetails: React.FC<IProps> = ({ userId, favorites, removeF
 
         <button
           className="p-1"
+          onClick={handleFavoriteToggle}
         >
           {isFavorite ? (
             <StarFavoriteIcon width={16} height={16} />
@@ -99,12 +102,17 @@ export const CryptoItemDetails: React.FC<IProps> = ({ userId, favorites, removeF
         </button>
       </div>
 
-      {/*{detailsData.markets_coin_data && (*/}
-      {/*  <DetailsCoinsData cryptoMarketCoinData={detailsData.markets_coin_data} />*/}
-      {/*)}*/}
-      {/*{detailsData.markets.length > 0 && (*/}
-      {/*  <DetailsMarketsData cryptoMarketsData={detailsData.markets} />*/}
-      {/*)}*/}
+      {detailsData.markets_coin_data && (
+        <React.Suspense fallback={<div>Loading Coin Data...</div>}>
+          <DetailsCoinsData cryptoMarketCoinData={detailsData.markets_coin_data} />
+        </React.Suspense>
+      )}
+
+      {detailsData.markets?.length > 0 && (
+        <React.Suspense fallback={<div>Loading Market Data...</div>}>
+          <DetailsMarketsData cryptoMarketsData={detailsData.markets} />
+        </React.Suspense>
+      )}
     </CryptoModal>
   )
 }
